6325cc74cb8406fb3551f26145d9cc4ac28d90e8
Guiany Osvaldo Fokou <guiany-osvaldo.fokou@grenoble-inp.org>
2020-02-20 18:28:26 +0100

    calculs dz l'estimateur et de la variance des residus

--- worksheet3.R
+++ worksheet3.R
@@ -10,11 +10,12 @@ curve(courbe_lin, from=0, to=8 ,n=81)
 
 x3 = x^3
 x2 <- x^2
-x0 <- c(b[1])
-dim(x0) <- 81
-myset = data.frame(x3, x2, x, b[1])
+x0 <- vector("numeric", 81)
+x0<- x0+b[1]
+#dim(x0) <- 81
+myset = data.frame(x3, x2, x, x0)
 
-vals = b[1]+b[2]*x+b[3]*x^2+b[4]*x^3
+#vals = x0+b[2]*x+b[3]*x^2+b[4]*x^3
 
 lm_x = lm(y ~ ., myset)
 coef(lm_x ~ res)
@@ -23,3 +24,8 @@ plot(x, y, col="red")
 curve(courbe_lin, from=0, to=8 ,n=81, add=TRUE)
 curve(courbe_sans_sig, from=0, to=8 ,n=81, add=TRUE, col="green")
 
+# calculer la variance des residuels 
+var(lm_x$residuals)
+
+# compute confidence interval of significance level a = 5%
+confint(lm_x, level=0.95)
